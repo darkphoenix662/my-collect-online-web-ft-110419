3f5a8524432c6e16119d30ad9cd347a02905ea85
Sean Good <tree662@protonmail.com>
2019-11-12 03:23:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,1 +1,19 @@
-['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(collection)
+  i = 0
+  empty_collection = []
+  while i < collection.length 
+    yield(collection[i])
+    i += 1 
+  end
+  collection
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
+
+
+# my_collect(collection) do |student|
+#   students.split(" ").first 
+# end
